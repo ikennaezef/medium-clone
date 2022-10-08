b72c5b7c97dfbf378da66babfb4dd5eb3635e595
Ikenna <iykee0333@gmail.com>
2022-10-08 23:59:25 +0100

    query and isr for post detail page

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,7 +1,72 @@
+import { GetStaticProps } from "next";
 import React from "react";
+import { sanityClient } from "../../lib/sanity";
+import { Post } from "../../types";
 
-const PostDetail = () => {
+interface Props {
+	post: Post;
+}
+
+const PostDetail = ({ post }: Props) => {
+	console.log(post);
 	return <div>PostDetail</div>;
 };
 
 export default PostDetail;
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+	const query = `*[_type=="post" && slug.current == $slug][0]{
+		_id,
+		_createdAt,
+		title,
+		author -> {
+		 name,
+		 image,
+		},
+		description,
+		mainImage,
+		body,
+		slug{
+		 current,
+	 }
+	}`;
+
+	const post = await sanityClient.fetch(query, {
+		slug: params?.slug,
+	});
+
+	if (!post) {
+		return {
+			notFound: true,
+		};
+	}
+
+	return {
+		props: { post },
+		revalidate: 300,
+	};
+};
+
+export const getStaticPaths = async () => {
+	const query = `*[_type=="post"]{
+		_id,
+    slug {
+      current
+    }
+  }`;
+
+	const posts = await sanityClient.fetch(query);
+
+	const paths = posts.map((post: Post) => {
+		return {
+			params: {
+				slug: post.slug.current,
+			},
+		};
+	});
+
+	return {
+		paths,
+		fallback: "blocking",
+	};
+};
